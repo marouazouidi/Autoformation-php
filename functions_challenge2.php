b84e093fdb22720ac9f2216ae5459ca3cabee79e
marouazouidi <155592683+marouazouidi@users.noreply.github.com>
2026-03-10 15:56:53 +0100

    functions_challenge2.php

--- functions_challenge2.php
+++ functions_challenge2.php
@@ -1,8 +1,9 @@
 <?php 
 function calculateArea($width, $height){
-    $area = $width/$height;
+    $area = $width*$height;
     return $area;
 }
 $totalArea = calculateArea(40,10);
 echo " The total area is " . $totalArea . " square units."
+
 ?>
